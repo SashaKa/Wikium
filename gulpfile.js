a8c77ade84bdc327b15c02cce52b3b93d63cd15e
SashaKa <skrableskru@gmail.com>
2017-07-14 10:32:37 +0300

    some html

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,76 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    plugins = require('gulp-load-plugins')({
-        rename: {
-            'gulp-live-server': 'serve'
-        }
-    });
+const lessToScss = require("gulp-less-to-scss");
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+const plugins = require("gulp-load-plugins")({
+  rename: {
+    "gulp-live-server": "serve"
+  }
+});
+const sass = require('gulp-sass');
 
-gulp.task('default', ['watch']);
+gulp.task("default", ["watch"]);
 
-gulp.task('build-css', function () {
-    return gulp.src('src/less/*.less')
-        .pipe(plugins.plumber())
-        .pipe(plugins.less())
-        .on('error', function (err) {
-            gutil.log(err);
-            this.emit('end');
-        })
-        .pipe(plugins.autoprefixer({
-            browsers: [
-                    '> 1%',
-                    'last 2 versions',
-                    'firefox >= 4',
-                    'safari 7',
-                    'safari 8',
-                    'IE 8',
-                    'IE 9',
-                    'IE 10',
-                    'IE 11'
-                ],
-            cascade: false
-        }))
-        .pipe(gulp.dest('build/css')).on('error', gutil.log);
+gulp.task("build-css-from-less", function() {
+  return gulp
+    .src("src/less/*.less")
+    .pipe(plugins.plumber())
+    .pipe(plugins.less())
+    .on("error", function(err) {
+      gutil.log(err);
+      this.emit("end");
+    })
+    .pipe(
+      plugins.autoprefixer({
+        browsers: [
+          "> 1%",
+          "last 2 versions",
+          "firefox >= 4",
+          "safari 7",
+          "safari 8",
+          "IE 8",
+          "IE 9",
+          "IE 10",
+          "IE 11"
+        ],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("build/css"))
+    .on("error", gutil.log);
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/less/**/*.less', ['build-css']);
+gulp.task("build-css-from-scss", function() {
+  return gulp
+    .src("src/scss/*.scss")
+    .pipe(plugins.plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      plugins.autoprefixer({
+        browsers: [
+          "> 1%",
+          "last 2 versions",
+          "firefox >= 4",
+          "safari 7",
+          "safari 8",
+          "IE 8",
+          "IE 9",
+          "IE 10",
+          "IE 11"
+        ],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("build/css"))
+    .on("error", gutil.log);
+});
+
+gulp.task("lessToScss", function() {
+  gulp
+    .src("src/less/**/*.less")
+    .pipe(lessToScss())
+    .pipe(gulp.dest("src/scss"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("src/less/**/*.less", ["build-css-from-less"]);
 });
